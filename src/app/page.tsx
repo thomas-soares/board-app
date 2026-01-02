79d57a6d62efea6c5f1811aeaba99538190f7502
Thomas Soares <thomas__soares@hotmail.com>
2026-01-02 10:35:30 -0300

    chore: create card and button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { ArchiveIcon } from "lucide-react";
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
 import { Section } from "@/components/section";
+import { Card } from "@/components/card";
 
 export default function Home() {
   return (
@@ -19,7 +20,28 @@ export default function Home() {
 
           {/* Content */}
           <Section.Content>
-            <div>card 1</div>
+            <Card.Root>
+              <Card.Header>
+                <Card.Number>ECO-301</Card.Number>
+                <Card.Title>Implementar cartão de crédito</Card.Title>
+              </Card.Header>
+              <Card.Footer>
+                <button
+                  type="button"
+                  className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer"
+                >
+                  <ThumbsUpIcon className="size-3" />
+                  <span className="text-sm">12</span>
+                </button>
+                <button
+                  type="button"
+                  className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer"
+                >
+                  <MessageCircleIcon className="size-3" />
+                  <span className="text-sm">6</span>
+                </button>
+              </Card.Footer>
+            </Card.Root>
             <div>card 2</div>
             <div>card 3</div>
           </Section.Content>
